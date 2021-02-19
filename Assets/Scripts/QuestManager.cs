967a0d055a451c37306c08065ca998d33896f95c
TSKsmiley <37146398+TSKsmiley@users.noreply.github.com>
2021-02-19 14:57:15 +0100

    Quest stuff (19/02/21)

---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using QuestSystem;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Quest.AllQuests[0].Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
