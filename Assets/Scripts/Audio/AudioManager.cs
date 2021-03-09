ac6a949cb4b9b6196194fed6b1bc1bec6deb2c9f
TSKsmiley <37146398+TSKsmiley@users.noreply.github.com>
2021-03-09 12:28:25 +0100

    hammer and my progress so far

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } 
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.spatialBlend = s.spatialBlend;
+
+            s.source.loop = s.loop;
+            
+        }
+    }
+
+    public AudioSource GetSound(string name)
+    {
+        Sound s = FindSound(name);
+
+        return s.source;
+    }
+    
+    public void Play(string name)
+    {
+        Sound s = FindSound(name);
+            
+        s.source.Play();
+    }
+
+    public void Stop(string name) {
+        Sound s = FindSound(name);
+
+        s.source.Stop();
+    }
+
+    private Sound FindSound(string _name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == _name);
+
+        if (s == null)
+        {
+            Debug.LogWarning("Sound " + name + " not found");
+        }
+
+        return s;
+    }
+}
--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -9,12 +9,13 @@ public class QuestManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Quest.StartNew("Space", "do you even space bro", "press space to complete this test quest");
         Quest.AllQuests[0].Start();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)) Quest.CompleteActive();
     }
 }
