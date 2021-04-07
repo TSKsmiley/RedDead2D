d4d2b738f021043ad26ccd0b478ac089355e6835
TSKsmiley <37146398+TSKsmiley@users.noreply.github.com>
2021-04-07 11:43:01 +0200

    Inv

--- Assets/Scripts/Inventory/Controller.cs
+++ Assets/Scripts/Inventory/Controller.cs
@@ -1,32 +1,55 @@
-﻿using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using static Inventory.Interfaces;
 
 namespace Inventory
 {
     public class Controller : MonoBehaviour
     {
-        public Item[] Inventory = new Item[20];
+        public static int InventorySize = 35;
+        public GameObject UIObjectsParrent;
+        public List<ItemObject> UIObjects = new List<ItemObject>();
+        public ItemStack[] Inventory = new ItemStack[InventorySize];
 
-        public bool hasItem(Item item)
+        public void Start()
         {
-            foreach (Item currItem in Inventory)
+            foreach (var item in UIObjectsParrent.GetComponentsInChildren<ItemObject>())
             {
-                if (currItem.name == item.name) return true;
+                UIObjects.Add(item);
+            }
+            refreshUI();
+        }
+
+        public bool hasItem(IItem item)
+        {
+            foreach (ItemStack currItem in Inventory)
+            {
+                if (currItem.Item.name == item.name) return true;
             }
 
             return false;
         }
 
-        public List<int> findItem(Item item)
+        public List<int> findItem(IItem item)
         {
             List<int> results = new List<int>();
             for (int i = 0; i < Inventory.Length; i++)
             { 
-                if (Inventory[i].name == item.name) results.Add(i);
+                if (Inventory[i].Item.name == item.name) results.Add(i);
             }
 
             return results;
+        }
+
+        public void refreshUI()
+        {
+            for (int i = 0; i < InventorySize; i++)
+            {
+                if(Inventory[i] == null)
+                UIObjects[i].Item = Inventory[i].Item;
+                UIObjects[i].UpdateUI();
+            }
         }
         
     }
