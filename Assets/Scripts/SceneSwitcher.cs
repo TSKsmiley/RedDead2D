a8ef10ffb3d0dc33d25430f58e7eed0b4f2ff0c3
BenjaminHvidberg <Benjaminboegelhvidberg@gmail.com>
2021-04-20 23:14:08 +0200

    Added Main Menu

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+
+	private float transitionTime = 1F;
+
+	public GameObject fadeToBlack;
+
+	
+	public void StartGameClick()
+	{
+		fadeToBlack.SetActive(true);
+		StartCoroutine(levelFade());
+	}
+
+	public void QuitGameClick()
+	{
+		Application.Quit();
+	}
+
+
+
+	IEnumerator levelFade()
+	{
+		yield return new WaitForSeconds(transitionTime);
+
+		SceneManager.LoadScene(1);
+	}
+}
