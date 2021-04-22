86fa7153908e0c708d3952b7afed34d02810f627
FredTheNoob <frederikhammer02@gmail.com>
2021-04-22 16:07:26 +0200

    Added shop, added shop vendor tyrone, fixed issue where you couldnt heal all the way up with consumerables. Consumerables now gets removed after use

--- Assets/Scripts/Inventory/Items/Item Types/Derived/ConsumableItem.cs
+++ Assets/Scripts/Inventory/Items/Item Types/Derived/ConsumableItem.cs
@@ -5,14 +5,13 @@ namespace Inventory
     [CreateAssetMenu(fileName = "ConsumerableItem", menuName = "Items/ConsumerableItem")]
     public class ConsumableItem : Item
     {
-        [field: SerializeField]
-        public int healthRegen {get;set;}
+        public int healthRegen;
         
         public override void Use(InputMaster _playerInput, GameObject _caller)
         {
             Debug.Log("consumable");
             ApplyHealthRegen(_caller.GetComponent<PlayerController>());
-            
+
         }
         
         public override void ControllerUse(InputMaster _playerInput, GameObject _caller)
@@ -24,10 +23,19 @@ namespace Inventory
 
         public void ApplyHealthRegen(PlayerController player)
         {
-            int healthToAdd = healthRegen - player.currHealth;
-            player.currHealth += healthToAdd;
-            
+            if (player.currHealth == 100) return;
+            int healthToAdd = healthRegen + player.currHealth;
+            if (healthToAdd > 100)
+            {
+                player.currHealth += player.maxHealth - player.currHealth;
+            }
+            else
+            {
+                player.currHealth += healthRegen;
+            }
+
             player.healthBar.SetHealth(player.currHealth);
+            Inventory.Controller.instance.RemoveItems(1, this.name);
         }
     }
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -144,6 +144,10 @@ public class PlayerController : MonoBehaviour
 
                 diagTrigger.TriggerDialogue(diag);
                 break;
+            
+            case "ShopVendor":
+                currentTrigger.gameObject.GetComponent<ShopVendor>().OpenShop(rb);
+                break;
         }
     }
 
